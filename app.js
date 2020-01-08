a5ef2163af9bdf642a2a7a06ac9f34b07810fc2e
Kennedy Njeri <mistakenz123@gmail.com>
2020-01-08 20:06:05 +0300

    customizing http requests

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const request = require("request")
 
 const url = 'https://api.darksky.net/forecast/1a2ac66cd0792407548da2d4da7e2301/37.8267,-122.4233'
 
-request({ url:url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently.time)
+// json sets body to json representation
+request({ url:url, json: true }, (error, response) => {
+    let degrees = response.body.currently.temperature
+    let chance = response.body.currently.precipProbability
+    let today = response.body.daily.data[0].summary
+    console.log(`${today}It is currently ${degrees} degrees out. There is a ${chance}% chance of rain`)
+    // const data = JSON.parse(response.body)
+    // console.log(data.currently.time)
 })
